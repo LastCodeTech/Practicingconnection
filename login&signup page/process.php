50c668de236e8533f1132e90ba4e941ef2b37809
= <sulaimonabubakre260@gmail.com>
2025-10-27 16:51:37 +0100

    ffhhvhhv

--- login&signup page/process.php
+++ login&signup page/process.php
@@ -1,4 +1,14 @@
 <?php
+include_once('server.php')
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $username = htmlspecialchars($_POST['']);
+    $loginUname = htmlspecialchars($_POST['loginUsername']);
+    $loginPassword = htmlspecialchars($_POST['loginPassword']);
+    $signupUname = htmlspecialchars($_POST['signupUsername']);
+    $signupPassword = htmlspecialchars($_POST['signupPassword']);
+    $signupConfirmPassword = htmlspecialchars($_POST['signupConfirmPassword']);
+
+  
+}
+else{
+    header('location:signup.php')
 }
